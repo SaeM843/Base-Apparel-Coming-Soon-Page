ea399973e34e8addba64d1cd4a581ebf2f0b63d4
Sae Matsuda <sae.matsuda.843@gmail.com>
2023-06-22 16:36:47 +0900

    added JJS functions

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const submit = document.querySelector(".submit-btn");
 const errorText = document.querySelector(".error-msg");
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const errorIcon = document.querySelector(".error-icon");
 
 form.addEventListener("submit", validate);
 submit.addEventListener("click", validate);
@@ -14,5 +15,16 @@ function validate(e) {
 
     if(inputValue === "") {
         errorText.style.display = "block";
+        errorIcon.style.display = "block"
+    } else if (!isEmail(inputValue)) {
+        errorText.style.display = "block";
+        errorIcon.style.display = "block"
+    } else {
+        errorText.style.display = "none";
+        errorIcon.style.display = "none"
     }
 }
+
+function isEmail(input) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)
+  }
